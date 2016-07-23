55454397ee80758c82de7c07c78f67d2a05e0264
t430i <you@example.com>
2016-07-23 23:45:47 +0800

    foo

--- app/views.lua
+++ app/views.lua
@@ -105,12 +105,10 @@ function m.qq(request, kwargs)
     local user = User:get{openid=openid}
     if not user then
         local data = qq:get_user_info(openid, token)
-        log('raw user:', data)
         user = User:create{openid=openid, username=data.username, avatar=data.avatar}
     end
-    log('user:', user)
     request.session.user = user
-    return response.Template("home.html", {messages = {'成功通过qq登陆'}})
+    return response.Redirect('/profile')
     --return response.Plain(string.format('url:%s, \ncode:%s,\n token:%s,\n openid:%s, \nuser:%s', repr(qq), code, token, openid, repr(user)))
 end
 function m.github(request, kwargs)
