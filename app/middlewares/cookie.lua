dd0bdcef0aad8477667dc9eee5249539e07cd444
Your Name <you@example.com>
2016-07-27 16:09:42 +0800

    foo

--- app/middlewares/cookie.lua
+++ app/middlewares/cookie.lua
@@ -3,5 +3,6 @@ local function before(req, kwargs)
 end
 local function after(req, kwargs)
     req.cookie:_save()
+    loger('cookie, ', ngx.header['Set-Cookie'])
 end
 return { before = before, after = after}
