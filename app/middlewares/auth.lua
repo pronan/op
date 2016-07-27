040c876916c5bd4e97c0a4200efc843cb439d8eb
Your Name <you@example.com>
2016-07-27 15:58:09 +0800

    foo

--- app/middlewares/auth.lua
+++ app/middlewares/auth.lua
@@ -1,7 +1,7 @@
 local Query = require"resty.model".RawQuery
 
 local function before(req, kwargs)
-    loger('session:', req.session)
+    loger('session:', getmetatable(req.session))
     req.user = req.session.user
 end
 
