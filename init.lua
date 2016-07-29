ba5d0c9342f2391f5d3bf2748f4895b6288051d8
Your Name <you@example.com>
2016-07-29 15:33:44 +0800

    foo

--- init.lua
+++ init.lua
@@ -3,21 +3,20 @@ loger = utils.loger
 repr = utils.repr
 
 settings = require"settings"
-for k,v in pairs(settings) do
-    if k == 'COOKIE' or k == 'SESSION' then
-        v.expires = utils.simple_time_parser(v.expires)
-    elseif k == 'MIDDLEWARES' then
-    	local MIDDLEWARES = {}
-    	local MIDDLEWARES_REVERSED = {}
-    	local len = #v
-    	for i, m in ipairs(v) do
-		    if type(m) == 'string' then
-		        m = require(m)
-		    end
-		    MIDDLEWARES[i] = m
-		    MIDDLEWARES_REVERSED[len-i+1] = m
-    	end
-    	settings.MIDDLEWARES = MIDDLEWARES
-    	settings.MIDDLEWARES_REVERSED = MIDDLEWARES_REVERSED
-    end
+for i, k in ipairs{'COOKIE', 'SESSION'} do
+    local v = settings[k]
+    v.expires = utils.simple_time_parser(v.expires)
 end
+local v = settings.MIDDLEWARES
+local MIDDLEWARES = {}
+local MIDDLEWARES_REVERSED = {}
+local len = #v
+for i, m in ipairs(v) do
+    if type(m) == 'string' then
+        m = require(m)
+    end
+    MIDDLEWARES[i] = m
+    MIDDLEWARES_REVERSED[len-i+1] = m
+end
+settings.MIDDLEWARES = MIDDLEWARES
+settings.MIDDLEWARES_REVERSED = MIDDLEWARES_REVERSED
