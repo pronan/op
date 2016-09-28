ca09cb23c08ee1495340213873e98f55ced115ec
zhuanyenan <smaller3@163.com>
2016-09-28 01:21:29 +0000

    foo

--- hello.c
+++ hello.c
@@ -5,7 +5,8 @@ int main( void )
 {
      time_t second = 1474728477;
      struct tm result;
-     gmtime_r(&second, &result);
-     printf("%d", result.tm_year);
+     localtime_r(&second, &result);
+     printf("%d\n", result.tm_hour);
+     printf("%d", (int)mktime(&result));
      return 0;
 }
--- resty/mvc/datetime.lua
+++ resty/mvc/datetime.lua
@@ -1,9 +1,9 @@
 local ffi = require("ffi")
 ffi.cdef[[
-typedef uint32_t time_t;
+typedef int64_t  time_t;
 typedef struct tm {
-  int second;        /* Seconds. [0-60] (1 leap second) */
-  int minute;        /* Minutes. [0-59] */
+  int sec;        /* secs. [0-60] (1 leap sec) */
+  int min;        /* mins. [0-59] */
   int hour;          /* Hours.   [0-23] */
   int day;           /* Day.     [1-31] */
   int month;         /* Month.   [0-11] */
@@ -11,7 +11,7 @@ typedef struct tm {
   int wday;          /* Day of week. [0-6] */
   int yday;          /* Days in year.[0-365] */
   int isdst;         /* DST.     [-1/0/1]*/
-  long int gmtoff;   /* Seconds east of UTC.  */
+  long int gmtoff;   /* secs east of UTC.  */
   const char* zone;  /* Timezone abbreviation.  */
 } tm;
 struct tm* gmtime_r   (const time_t*, struct tm*);
@@ -35,27 +35,92 @@ local tm_ptr_constructor = ffi.typeof("struct tm[1]")
 local time_t_ptr_constructor = ffi.typeof("time_t[1]")
 local uint8_t_ptr_constructor = ffi.typeof("uint8_t[?]")
 
+
+
+-- local function gmtime(timestamp)
+--     local timestamp_ptr = ffi.new("time_t[1]", timestamp)
+--     local tm_ptr = ffi.new("struct tm[1]")
+--     ffi.C.gmtime_r(timestamp_ptr, tm_ptr)
+--     local  r = tm_ptr[0]
+--     return {
+--         year  = r.year+1900, 
+--         month = r.month+1,
+--         day   = r.day,
+--         hour  = r.hour,
+--         min= r.min,
+--         sec= r.sec,
+--     }
+-- end
+-- local function localtime(timestamp)
+--     local timestamp_ptr = ffi.new("time_t[1]", timestamp)
+--     local tm_ptr = ffi.new("struct tm[1]")
+--     ffi.C.localtime_r(timestamp_ptr, tm_ptr)
+--     local  r = tm_ptr[0]
+--     return {
+--         year  = r.year+1900, 
+--         month = r.month+1,
+--         day   = r.day,
+--         hour  = r.hour,
+--         min= r.min,
+--         sec= r.sec,
+--     }
+-- end
+
 local function gmtime(timestamp)
     local timestamp_ptr = time_t_ptr_constructor(timestamp)
     local tm_ptr = tm_ptr_constructor()
     ffi.C.gmtime_r(timestamp_ptr, tm_ptr)
-    return tm_ptr[0]
+    local  r = tm_ptr[0]
+    return {
+        year  = r.year+1900, 
+        month = r.month+1,
+        day   = r.day,
+        hour  = r.hour,
+        min= r.min,
+        sec= r.sec,
+    }
 end
 local function localtime(timestamp)
     local timestamp_ptr = time_t_ptr_constructor(timestamp)
     local tm_ptr = tm_ptr_constructor()
     ffi.C.localtime_r(timestamp_ptr, tm_ptr)
-    return tm_ptr[0]
+    local  r = tm_ptr[0]
+    return {
+        year  = r.year+1900, 
+        month = r.month+1,
+        day   = r.day,
+        hour  = r.hour,
+        min= r.min,
+        sec= r.sec,
+    }
 end
-local function mktime(tm)
-    local tm_ptr = tm_ptr_constructor(tm)
+
+local function mktime(r)
+    local tm = ffi.new("struct tm", {
+        year  = r.year-1900, 
+        month = r.month-1,
+        day   = r.day,
+        hour  = r.hour,
+        min   = r.min,
+        sec   = r.sec,
+    })
+    local tm_ptr = ffi.new("struct tm[1]",tm)
+    return ffi.C.mktime(tm_ptr)
+end
+local function gmtime_ptr(timestamp)
+    local timestamp_ptr = time_t_ptr_constructor(timestamp)
+    local tm_ptr = tm_ptr_constructor()
+    ffi.C.gmtime_r(timestamp_ptr, tm_ptr)
+    return tm_ptr
+end
+local function mktime_ptr(tm_ptr)
     return ffi.C.mktime(tm_ptr)
 end
 local function asctime(tm)
     -- 25 seems to be the min number that doesn't cause segmentation fault,
     -- here use 26 for safety.
-    local buf = uint8_t_ptr_constructor(26)
-    local tm_ptr = tm_ptr_constructor(tm)
+    local buf = ffi.new("uint8_t[?]", 26)
+    local tm_ptr = ffi.new("struct tm[1]")
     ffi.C.asctime_r(tm_ptr, buf)
     return ffi.string(buf)
 end
@@ -67,10 +132,34 @@ local function zerofill(num)
 end
 local function strfmt(tm)
   return string.format('%d-%s-%s %s:%s:%s', 
-      tm.year+1900, 
-      zerofill(tm.month+1), 
+      tm.year, 
+      zerofill(tm.month), 
       zerofill(tm.day), 
       zerofill(tm.hour), 
-      zerofill(tm.minute), 
-      zerofill(tm.second))
+      zerofill(tm.min), 
+      zerofill(tm.sec))
 end
+
+local t = 147472847
+
+print(t)
+print(strfmt(gmtime(t)))
+d = localtime(t)
+dp = gmtime_ptr(t)
+print(strfmt(d))
+print()
+
+local times=300000
+t1=os.time()
+for i=1,times do
+    local a=os.time(d)
+end
+t2=os.time()
+for i=1,times do
+    local a=mktime(d)
+end
+t3=os.time()
+print(mktime(d)==os.time(d))
+print(t2-t1)
+print(t3-t2)
+
